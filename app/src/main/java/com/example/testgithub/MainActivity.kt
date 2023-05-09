b6df1cf6f7ffcf02b2d63c79df4ef6a64c6ee730
AnasAlhussain <anasalhussain.sweeden@hotmail.com>
2023-05-09 14:13:01 +0200

    One Line Add

--- app/src/main/java/com/example/testgithub/MainActivity.kt
+++ app/src/main/java/com/example/testgithub/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private lateinit var Binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
